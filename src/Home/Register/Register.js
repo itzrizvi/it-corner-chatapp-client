9fce08430838694aa3a149018fa85e5e6db0aa8d
itzrizvi <shahriar.me.work@gmail.com>
2021-12-17 23:35:42 +0600

    Register User UI

--- src/Home/Register/Register.js
+++ src/Home/Register/Register.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
-
-
+    const [registerData, setRegisterData] = useState({});
+    const handleRegValues = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newRegistrationData = { ...registerData };
+        newRegistrationData[field] = value;
+        setRegisterData(newRegistrationData);
+    }
     const regitrationHandler = (e) => {
         e.preventDefault();
+        if (registerData.passwordOne !== registerData.passwordTwo) {
+            alert('Password Didn\'t Matched!!!');
+            return;
+        }
+        console.log(registerData);
     }
 
     return (
         <div>
             <h2>Register Here</h2>
             <form onSubmit={regitrationHandler} >
-                <input type="text" id="register-first-name" placeholder='Enter Your First Name' /> <br />
-                <input type="text" id="register-last-name" placeholder='Enter Your Last Name' /><br />
-                <input type="email" id="register-email" placeholder='Enter Your Email' /><br />
-                <input type="password" id="register-user-pass" placeholder='Enter Your Password' /><br />
-                <input type="password" id="re-register-user-pass" placeholder='Re-enter Your Password' /><br />
+                <input type="text" onBlur={handleRegValues} name='firstName' id="register-first-name" placeholder='Enter Your First Name' /> <br />
+                <input type="text" onBlur={handleRegValues} name='lastName' id="register-last-name" placeholder='Enter Your Last Name' /><br />
+                <input type="email" onBlur={handleRegValues} name='email' id="register-email" placeholder='Enter Your Email' /><br />
+                <input type="password" onBlur={handleRegValues} name='passwordOne' id="register-user-pass" placeholder='Enter Your Password' /><br />
+                <input type="password" onBlur={handleRegValues} name='passwordTwo' id="re-register-user-pass" placeholder='Re-enter Your Password' /><br />
                 <button type='submit'>Register</button>
             </form>
         </div>
